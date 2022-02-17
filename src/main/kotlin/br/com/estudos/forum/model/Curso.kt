54ce897c869da3fe3c1f0aa72c09d157e3a664b0
rafahlanutti <rafahlanutti@gmail.com>
2022-02-16 21:23:07 -0300

    Adicionando migrations

--- src/main/kotlin/br/com/estudos/forum/model/Curso.kt
+++ src/main/kotlin/br/com/estudos/forum/model/Curso.kt
@@ -7,8 +7,7 @@ import javax.persistence.Id
 
 @Entity
 data class Curso(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val nome: String,
     val categoria: String
